40b6f39f8436a5cde20757ef443ffec8621a1e56
NAVNEET ANAND <navneetanand037@gmail.com>
2026-02-17 23:40:39 +0530

    Initial backend structure: add models, middleware, and entry point

---
+++ e-commerce backend/models/user.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter Name"],
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Please enter Email"],
+      validate: [validator.isEmail, "Please enter valid Email"],
+    },
+
+    photo: {
+      type: String,
+      required: [true, "Please enter Photo"],
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+      required: [true, "Please enter Gender"],
+    },
+
+    dob: {
+      type: Date,
+      required: [true, "Please enter Date of birth"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+schema.virtual("age").get(function(){
+    const today = new Date();
+  const dob = this.dob;
+  let age = today.getFullYear() - dob.getFullYear();
+
+  if (
+    today.getMonth() < dob.getMonth() ||
+    (today.getMonth() === dob.getMonth() && today.getDate() < dob.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+})
+export const User = mongoose.model("User", schema);
